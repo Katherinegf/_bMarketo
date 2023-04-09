83fb89b7eeddec4b534d9683511d9d5fe05ed55d
Katherinegf <110284567+Katherinegf@users.noreply.github.com>
2023-04-09 16:53:29 +0200

    Add project files.

---
+++ WebbApp/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebbApp.Controllers
+{
+
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Products";
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            ViewData["Title"] = "Search for products";
+            return View();
+        }
+    }
+}
